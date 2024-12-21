f613a52c09822564a44b268972821786a1ca4e09
Samandar <samandartoshpulatov0704@gmail.com>
2024-12-21 11:26:55 +0500

    Initial Commit

---
+++ ForIOSReview/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  ForIOSReview
+//
+//  Created by Samandar on 21/12/24.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
